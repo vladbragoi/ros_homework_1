563f7c0c4daf2ebe94042c1bfbbb6c36cbab28ca
vlad <vlad.bragoi@gmail.com>
2018-04-06 18:33:02 +0200

    modified for starting roscore in background

--- launch/launch.sh
+++ launch/launch.sh
@@ -3,4 +3,3 @@ sleep 1
 gnome-terminal -x sh -c "rosrun ros_homework_1 visualizer"
 gnome-terminal -x sh -c "rosrun ros_homework_1 selector"
 gnome-terminal -x sh -c "rosrun ros_homework_1 publisher"
-bg
