7a86d9b7a848d067938ed815b65b5929a0f5220d
vlad <vlad.bragoi@gmail.com>
2018-04-06 16:52:14 +0200

    launch script (using gnome-terminal)

---
+++ launch/launch.sh
@@ -0,0 +1,6 @@
+roscore &
+sleep 1
+gnome-terminal -x sh -c "rosrun ros_homework_1 visualizer"
+gnome-terminal -x sh -c "rosrun ros_homework_1 selector"
+gnome-terminal -x sh -c "rosrun ros_homework_1 publisher"
+bg
