c82060e9ae75f33acc129a66fb54c817de80dd60
vlad <vlad.bragoi@gmail.com>
2018-04-06 13:36:35 +0200

    added matrix of values

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,22 +1,38 @@
 #include "ros/ros.h"
 #include "ros_homework_1/Message.h"
 
+std::string matrix[4][3];
+
 int main(int argc, char **argv) {
-
     ros::init(argc, argv, "publisher");
-
     ros::NodeHandle n;
+    int row;
+
+    matrix[0][0] = "Mario";
+    matrix[0][1] = "22";
+    matrix[0][2] = "L. in Informatica";
+    matrix[1][0] = "Luigi";
+    matrix[1][1] = "25";
+    matrix[1][2] = "LM. in Matematica";
+    matrix[2][0] = "Cristina";
+    matrix[2][1] = "20";
+    matrix[2][2] = "L. in Medicina";
+    matrix[3][0] = "Laura";
+    matrix[3][1] = "24";
+    matrix[3][2] = "L. in Lingue";
 
     ros::Publisher message = n.advertise<ros_homework_1::Message>("message", 1000);
 
     ros::Rate loop_rate(1);;
 
+    int count = 1;
     while (ros::ok()) {
         ros_homework_1::Message msg;
         
-        msg.name = "Mario";
-        msg.age = 22;
-        msg.degree = "L. in Informatica";
+        row = (count + 4) % 4;
+        msg.name = matrix[row][0];
+        msg.age = std::atoi(matrix[row][1].c_str());
+        msg.degree = matrix[row][2];
         
         ROS_INFO("Name: %s Age: %i Degree: %s\n", msg.name.c_str(), (int)msg.age, msg.degree.c_str());
 
@@ -25,6 +41,7 @@ int main(int argc, char **argv) {
         ros::spinOnce();
 
         loop_rate.sleep();
+        count++;
     }
 
     return 0;
