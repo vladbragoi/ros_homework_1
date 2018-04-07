166875a42c89357fac0a9ae11640e6682dd942ee
vlad <vlad.bragoi@gmail.com>
2018-04-07 12:55:22 +0200

    changed launch order

--- launch/launch.sh
+++ launch/launch.sh
@@ -1,5 +1,5 @@
 roscore &
 sleep 1
+gnome-terminal -x sh -c "rosrun ros_homework_1 publisher"
 gnome-terminal -x sh -c "rosrun ros_homework_1 visualizer"
 gnome-terminal -x sh -c "rosrun ros_homework_1 selector"
-gnome-terminal -x sh -c "rosrun ros_homework_1 publisher"
